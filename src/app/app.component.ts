a15a2cfe83e5769bc3985883b46de2e2f70e86ad
Naipawat Poolsawat <tata-naipawat@hotmail.com>
2024-12-27 17:30:16 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Menubar } from 'primeng/menubar';
+import { LucideAngularModule, Bed, Info } from 'lucide-angular';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Menubar,
+    LucideAngularModule,
+    RouterModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'boraxpr-blog';
+  readonly BedIcon = Bed;
+  readonly InfoIcon = Info;
+  items: any[] = [
+    { label: 'Home', icon: 'bed', routerLink: '/' },
+    { label: 'About', icon: 'info', routerLink: '/about' },
+  ];
 }
