480b72be4e95c66a883addf76adbf9a576e70306
Naipawat Poolsawat <tata-naipawat@hotmail.com>
2024-12-28 17:50:07 +0700

    Fix : menu icon does not show - Material icon is a font. Therefore, it was overridden by global font selector.. I fixed it by state not mat-icon enforce global font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,19 +18,11 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  items = [
-    { name: 'Home', url: '/', isActive: true },
-    { name: 'About', url: '/about', isActive: false },
-    { name: 'Contact', url: '/contact', isActive: false },
-  ];
-
-  trackLink(index: number, link: any): number {
-    return link.url; // Track by URL or a unique property
-  }
   title = 'boraxpr-blog';
 }
